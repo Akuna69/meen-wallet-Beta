f9407ab4b7c9ef72c23fce0fd284c4582db8a436
Akuna69 <171538361+Akuna69@users.noreply.github.com>
2026-09-20 01:54:34 -0400

    Remove armv7-linux-android from ALL_TARGETS

--- libwallet/librs/makelibs.sh
+++ libwallet/librs/makelibs.sh
@@ -21,7 +21,7 @@ function _cargo() {
 set -x
 
 IOS_TARGETS="aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim"
-ALL_TARGETS="aarch64-unknown-linux-musl x86_64-unknown-linux-musl i686-unknown-linux-musl aarch64-apple-darwin x86_64-apple-darwin aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android $IOS_TARGETS"
+ALL_TARGETS="aarch64-unknown-linux-musl x86_64-unknown-linux-musl i686-unknown-linux-musl aarch64-apple-darwin x86_64-apple-darwin aarch64-linux-android x86_64-linux-android $IOS_TARGETS"
 
 # Allow callers to override the targets we'll build
 if [ -z "$TARGETS" ]; then
