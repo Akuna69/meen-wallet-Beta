6014032a159d2f05c11cb706b12162997a92d153
Akuna69 <171538361+Akuna69@users.noreply.github.com>
2026-09-23 01:36:04 -0400

    Update InAppUpdateManager.kt

--- android/apolloui/src/main/java/io/muun/apollo/presentation/ui/home/InAppUpdateManager.kt
+++ android/apolloui/src/main/java/io/muun/apollo/presentation/ui/home/InAppUpdateManager.kt
@@ -1,5 +1,17 @@
 package io.muun.apollo.presentation.ui.home
 
-open class InAppUpdateManager {
-    open fun checkForUpdate() {}
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.appcompat.app.AppCompatActivity
+
+interface InAppUpdateManager {
+
+    fun checkForUpdate()
+
+    fun interface Factory {
+        operator fun invoke(
+            activity: AppCompatActivity,
+            updateLauncher: ActivityResultLauncher<IntentSenderRequest>
+        ): InAppUpdateManager
+    }
 }
