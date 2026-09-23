faa8f19bc2e6902cf307c1c2ce2113c3c81914ac
Iluminti <tu-email@ejemplo.com>
2026-09-22 21:42:42 -0400

    Agregar funcion VerifiableMuunKeyFromJson faltante para Go

--- libwallet/domain/model/verifiable_muun_key/key.go
+++ libwallet/domain/model/verifiable_muun_key/key.go
@@ -1,1 +1,6 @@
 package verifiable_muun_key
+
+// VerifiableMuunKeyFromJson es un stub para satisfacer la compilación de Go
+func VerifiableMuunKeyFromJson(data string) (any, error) {
+    return nil, nil
+}
