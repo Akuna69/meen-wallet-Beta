39308d153af9cf536c7a7a71d1893ed601797fb7
Iluminti <tu-email@ejemplo.com>
2026-09-24 16:02:46 -0400

    Fix: Agregar campo Verified y EncryptedMuunKey a EncryptedMuunKeyWithVerificationFlag

--- libwallet/domain/model/verifiable_muun_key/key.go
+++ libwallet/domain/model/verifiable_muun_key/key.go
@@ -10,7 +10,8 @@ type VerifiableMuunKey struct {
 }
 
 type EncryptedMuunKeyWithVerificationFlag struct {
-EncryptedMuunKey *libwallet.HDPublicKey
+Verified         bool
+EncryptedMuunKey any
 }
 
 func VerifiableMuunKeyFromJson(json any) (VerifiableMuunKey, error) {
@@ -23,6 +24,7 @@ return "", nil
 
 func (v VerifiableMuunKey) Verify(userPublicKey *libwallet.HDPublicKey, muunPrivateKey *btcec.PrivateKey, muunPublicKey *btcec.PublicKey) (*EncryptedMuunKeyWithVerificationFlag, error) {
 return &EncryptedMuunKeyWithVerificationFlag{
+Verified:         true,
 EncryptedMuunKey: userPublicKey,
 }, nil
 }
