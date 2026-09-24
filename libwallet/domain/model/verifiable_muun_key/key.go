eb7c6877e65737e2090a20f61bbe20ca6cdf1341
Iluminti <tu-email@ejemplo.com>
2026-09-24 04:47:45 -0400

    Fix: Agregar palabra clave func y receptor a IntoJSON

--- libwallet/domain/model/verifiable_muun_key/key.go
+++ libwallet/domain/model/verifiable_muun_key/key.go
@@ -1,17 +1,17 @@
 package verifiable_muun_key
 
 type VerifiableMuunKey struct {
-    // Estructura base para satisfacer la compilación
+// Estructura base para satisfacer la compilación
 }
 
 func VerifiableMuunKeyFromJson(data string) (VerifiableMuunKey, error) {
-    return VerifiableMuunKey{}, nil
+return VerifiableMuunKey{}, nil
 }
 
-IntoJSON() (string, error) {
-    return "", nil
+func (v VerifiableMuunKey) IntoJSON() (string, error) {
+return "", nil
 }
 
 func (v VerifiableMuunKey) Verify() error {
-    return nil
+return nil
 }
